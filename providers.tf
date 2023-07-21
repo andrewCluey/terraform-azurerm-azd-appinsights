cc3ef584f282fd2de90283c771b660c27f703eec
andy <andy@andys-iMac.local>
2023-07-21 19:58:27 +0100

    initial commit

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "~>3.47.0"
+      source  = "hashicorp/azurerm"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "~>1.2.24"
+    }
+  }
+}
